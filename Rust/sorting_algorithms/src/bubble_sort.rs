a44012746473a7b81cc4ee49973e3b32b8753c72
qcandymanp <hendrikherpers@gmail.com>
2023-09-30 11:30:29 +0200

    Add BubbleSort in Rust

---
+++ Rust/sorting_algorithms/src/bubble_sort.rs
@@ -0,0 +1,9 @@
+pub fn sort(list: &mut [i32]) {
+    for i in 0..list.len() {
+        for j in 0..list.len() - i - 1 {
+            if list[j] > list[j + 1] {
+                list.swap(j, j + 1);
+            }
+        }
+    }
+}
