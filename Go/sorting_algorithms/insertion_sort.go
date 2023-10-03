ff6477217f4ff5eb18ce081e607f710e3d9061fe
Hendrik Herpers <hherpers@network-publishing.de>
2023-10-03 13:16:00 +0200

    Add InsertionSort in Go

---
+++ Go/sorting_algorithms/insertion_sort.go
@@ -0,0 +1,15 @@
+package sorting_algorithms
+
+func InsertionSort(list []int) []int {
+	for i := 0; i < len(list)-1; i++ {
+		if list[i] > list[i+1] {
+			list[i], list[i+1] = list[i+1], list[i]
+			for j := i; j > 0; j-- {
+				if list[j] < list[j-1] {
+					list[j], list[j-1] = list[j-1], list[j]
+				}
+			}
+		}
+	}
+	return list
+}
